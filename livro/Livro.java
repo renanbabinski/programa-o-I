fdd377551e85cf66cde27f191c4224fad6c77199
Aluno (local) <you@example.com>
2017-09-14 19:59:31 -0300

    novo objeto

--- livro/Livro.java
+++ livro/Livro.java
@@ -3,16 +3,23 @@ public class Livro{
   String descricao;
   double valor;
   String isbn;
-  String nomedoautor;
-  String emaildoautor;
-  String cpfdoautor;
+  Pessoa autor = new Pessoa();
+
+  class Pessoa{
+    String nome;
+    String email;
+    String cpf;
+  }
 
   public void mostrarDetalhes(){
     System.out.println("Nome do livro:" +this.nome);
     System.out.println("Descrição :" +this.descricao);
     System.out.println("Valor :" +this.valor);
     System.out.println("ISBN :" +this.isbn);
-    System.out.println("Nome do Autor:"+this.nomedoautor);
+    System.out.println("Nome do autor:" +this.autor.nome);
+    System.out.println("Email do autor:" +this.autor.email);
+    System.out.println("CPF do autor:" +this.autor.email);
+    System.out.println("");
   }
 
 
--- livro/Programa.java
+++ livro/Programa.java
@@ -6,27 +6,27 @@ public class Programa{
     lista[0].descricao = "Tecnologia";
     lista[0].valor = 100.00;
     lista[0].isbn = "1621101015";
-    lista[0].nomedoautor = "DanBrown";
-    lista[0].emaildoautor = "Dan@gmail.com";
-    lista[0].cpfdoautor = "085.213.279-43";
+    lista[0].autor.nome = "DanBrown";
+    lista[0].autor.email = "Dan@gmail.com";
+    lista[0].autor.cpf = "085.213.279-43";
 
     lista[1] = new Livro();
     lista[1].nome = "Senhor dos anéis";
     lista[1].descricao = "Ficção";
     lista[1].valor = 50.00;
     lista[1].isbn = "1621101017";
-    lista[1].nomedoautor = "Gandalf";
-    lista[1].emaildoautor = "Gandalf@gmail.com";
-    lista[1].cpfdoautor = "085.213.279-49";
+    lista[1].autor.nome = "Gandalf";
+    lista[1].autor.email = "Gandalf@gmail.com";
+    lista[1].autor.cpf = "085.213.279-49";
 
     lista[2] = new Livro();
     lista[2].nome = "Robin";
     lista[2].descricao = "akr";
     lista[2].valor = 15.00;
     lista[2].isbn = "999999";
-    lista[2].nomedoautor = "renan";
-    lista[2].emaildoautor = "renan@gmail.com";
-    lista[2].cpfdoautor = "07688986778";
+    lista[2].autor.nome = "renan";
+    lista[2].autor.email = "renan@gmail.com";
+    lista[2].autor.cpf = "07688986778";
 
     for (Livro l :lista ) {
       l.mostrarDetalhes();
