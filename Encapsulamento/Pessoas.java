8aae235fc51127d7031949c679aad67d62b302bd
renanbabinski <renan10babinski@hotmail.com>
2017-09-28 21:12:24 -0300

    add

---
+++ Encapsulamento/Pessoas.java
@@ -0,0 +1,12 @@
+import java.util.List
+public class{
+	private int idade;
+	private double peso;
+	private String nome;
+	ArrayList lista = new ArrayList();
+
+
+	public void getInfos(){
+		System.out.println()
+	}
+}
---
+++ Encapsulamento/Principal.java
@@ -0,0 +1,11 @@
+import java.util.Scanner
+public class Principal{
+	public static void main(String [] args){
+		int menu;
+		while(menu != 0){
+			System.out.println("MENU\n\n");
+			System.out.println
+		}
+
+	}
+}
